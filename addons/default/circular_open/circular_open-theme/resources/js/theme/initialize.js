96bb19b3f107b6f34f8412e482caa34bedf11737
Jem Lopez (Symbiote) <jem@symbiote.com.au>
2021-01-22 15:53:29 +1100

    feature(Carousel) Added slick carousel and feature carousel

--- addons/default/circular_open/circular_open-theme/resources/js/theme/initialize.js
+++ addons/default/circular_open/circular_open-theme/resources/js/theme/initialize.js
@@ -1,5 +1,37 @@
 (function (window, document) {
 
-    // Go!
-    
+    $('.carousel-wrapper').slick({
+        dots: true,
+        infinite: true,
+        speed: 300,
+        slidesToShow: 1,
+        autoplay: false,
+        arrows: false
+    });
+
+    $('.featured-carousel-wrapper').slick({
+        dots: true,
+        infinite: true,
+        speed: 300,
+        slidesToShow: 1,
+        autoplay: false,
+        arrows: false
+    });
+
+    function featureSlideAction() {
+        const links = document.querySelectorAll('.featured-slides__link');
+        for (let link of links) {
+            link.addEventListener("click", (e) => {
+                e.preventDefault();
+                if (link) {
+                    let slideIndex = parseInt(link.getAttribute('data-slide-index') || "0");
+                    $('.featured-carousel-wrapper').slick('slickGoTo', slideIndex - 1);
+                }
+            })
+        }
+
+    }
+
+    featureSlideAction()
+
 })(window, document);
